4e3a95a05d12548ff14351b294028b2df82e9854
teammixture <team@mixture.io>
2013-07-02 14:15:00 +0100

    Removed project path so as Mixture can add in automatically
    Updated the project path handling in Mixture so as improved for version controlled projects and no longer specific to a machine

--- compass/config.rb
+++ compass/config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 # -----------------------------------------------------------------------------
-project_path = "/Path"
+
 
 
 # Set this to the root of your project when deployed:
